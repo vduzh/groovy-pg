8b6049078eb8811406b5cbf5a00cafc7a6fdc453
vduzh <viktar.duzh@artezio.com>
2025-01-13 16:31:58 +0300

    Added placeholder for list tests

---
+++ src/test/groovy/by/vduzh/groovy/core/ListSpec.groovy
@@ -0,0 +1,11 @@
+package by.vduzh.groovy.core
+
+import spock.lang.Specification
+
+class ListSpec extends Specification {
+
+    def "create a list"() {
+        given: "a test that is not yet implemented"
+        throw new UnsupportedOperationException("Test not implemented yet")
+    }
+}
